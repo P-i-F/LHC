1889e175eab8b49d71590b79d72f20557d61c33a
P-i-F <spam2080@gmail.com>
2016-03-23 18:37:14 +0200

    Implemented command line parameters

--- Program.cs
+++ Program.cs
@@ -7,11 +7,25 @@ namespace LHC
 {
     class Program
     {
+        public static bool debugMode = false;
+
+        // available parameters (position based)
+        // [0] debug mode 0(default):no|1:yes
         static void Main(string[] args)
         {
             SimpleLog.SetLogFile(logDir: ".", prefix: "LHC_", writeText: false);
 
-            Console.WriteLine("Log is written to {0}.", SimpleLog.FileName);
+            if (args.Length > 0)
+            {
+                if (args[0] != null) {
+                    debugMode = (args[0] == "1");
+                }
+            }
+
+            string debugStatus = (debugMode ? "ON" : "OFF");
+            Console.WriteLine("Debug mode {0}", debugStatus);
+            SimpleLog.Info("Debug mode is " + debugStatus);
+            //Console.WriteLine("Log is written to {0}.", SimpleLog.FileName);
 
             SimpleLog.Info("Test logging started.");
             SimpleLog.Warning("This is a warning.");
